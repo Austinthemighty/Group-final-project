51d7b1867908f8b59169fe58a2644348b47b90fc
Angel Iniquez <angel.iniguez@west-mec.org>
2016-05-03 16:34:51 -0700

    setting up the PHP connection tests.

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=subscriptdb','root','root');
+}
+
+catch(Exception $e) {
+
+    echo "hello";
+}
+
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+
+
+
+</body>
+</html>
