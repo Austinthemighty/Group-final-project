41e85b6e4a42d8e5fcd515dedb67fde18b13a445
Carlos Moncada Garcia <carlos.garcia@west-mec.org>
2016-05-03 16:09:34 -0700

    The start on the css

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php
+
+
+
+
+
+
+
+
+
+?>
+
+<html>
+<head>
+    <title>
+        Contact Page
+    </title>
+</head>
+<body>
+ <h1>Contact</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
