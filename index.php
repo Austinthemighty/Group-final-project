89ebec8318c7db031272ad63d90320044c96273c
Angel Iniquez <angel.iniguez@west-mec.org>
2016-05-06 16:54:49 -0700

    Made the sign-up.php script and added the logo.

--- index.php
+++ index.php
@@ -3,20 +3,17 @@
 require_once('connect.php');
 
 ?>
-
-
 <!DOCTYPE html>
 
 <html>
 <head>
-
     <title>E-box</title>
     <title>Ebox</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <link rel="stylesheet" href="index.css">
     <script src="https://code.jquery.com/jquery-2.2.2.min.js" integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI=" crossorigin="anonymous"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet">
--- sign-up.php
+++ sign-up.php
@@ -1,34 +1,37 @@
 <?php
 
 session_start();
-   /* $title = 'Sign Up';
+
+    $title = 'Sign Up';
     require_once('connect.php');
     require_once('header.php');
 
     if (isset($_POST['submit'])) {
         // Grab the profile data from the POST
-        $username = trim($_POST['username']);
+        $id = trim($_POST['id']);
         $password1 = trim($_POST['password1']);
         $password2 = trim($_POST['password2']);
+        $email = trim($_POST['email']);
 
-        if (!empty($username) && !empty($password1) && !empty($password2) && ($password1 == $password2)) {
+        if (!empty($id) && !empty($password1) && !empty($email) && !empty($password2) && ($password1 == $password2)) {
             // Connect to the database
-            $dbh = new PDO("mysql:host=$db_hostname;$db_name, $db_username, $db_password");
+            $dbh = new PDO('mysql:host=localhost;dbname=e-box', 'root', 'root');
 
             // Make sure someone isn't already registered using this username
-            $query = "SELECT * FROM user WHERE username = :username";
+            $query = "SELECT * FROM user WHERE id = :id";
             $stmt = $dbh->prepare($query);
-            $stmt->execute(array('username' => $username));
+            $stmt->execute(array('id' => $id));
             $result = $stmt->fetchAll();
             $count = $stmt->rowCount();
 
             if ($count == 0) {
                 // The username is unique, so insert the data into the database
-                $query = "INSERT INTO user (username, password) VALUES (:username, SHA(:password))";
+                $query = "INSERT INTO user (id, email, password) VALUES (:id, :email, SHA(:password))";
                 $stmt = $dbh->prepare($query);
                 $stmt->execute(array(
-                    'username'  =>  $username,
-                    'password'=>  $password1
+                    'id'  =>  $id,
+                    'password'=>  $password1,
+                    'email' => $email
                 ));
 
                 // Confirm success with the user
@@ -46,24 +49,26 @@ session_start();
             echo '<p class="error">You must enter all of the sign-up data, including the desired password twice.</p>';
         }
     }
-   */
+
 
 
 
     ?>
 
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" value="<?php if (!empty($username)) echo $username; ?>" /><br />
-        <label for="password1">Password:</label>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" xmlns="http://www.w3.org/1999/html">
+        <label for="id">Username:</label>
+        <input type="text" id="id" name="id" value="<?php if (!empty($id)) echo $id; ?>" /><br />
+        <label for="password">Password:</label>
         <input type="password" id="password1" name="password1" /><br />
         <label for="password2">Password (retype):</label>
         <input type="password" id="password2" name="password2" /><br />
+        <label for="email">email:</label>
+        <input type="text" id="email" name="email" /></br>
     <input type="submit" value="Sign Up" name="submit" />
 </form>
 
 <?php
 
-echo "<p>hello, ". $_SESSION['user_id'] . "</p>";
+//echo "<p>hello, ". $_SESSION['user_id'] . "</p>";
 ?>
