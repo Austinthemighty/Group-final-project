6d0280dfdf7b1f964df2633c9a60ee2fae97fb0c
Austin Chopra <austin.chopra@west-mec.org>
2016-05-13 18:05:10 -0700

    worked on products page and some of the images

--- products.php
+++ products.php
@@ -106,15 +106,17 @@
         <div class="panel-heading">
           <h1>Basic</h1>
         </div>
-        <div class="panel-body">
+        <div class="panel-body" style="height: 558px">
+          <img  src="images/box-1.png" style="width: 400px; padding-bottom: 55px;">
           <p><strong>20</strong> Lorem</p>
           <p><strong>15</strong> Ipsum</p>
           <p><strong>5</strong> Dolor</p>
           <p><strong>2</strong> Sit</p>
           <p><strong>Endless</strong> Amet</p>
+
         </div>
         <div class="panel-footer">
-          <h3>$19</h3>
+          <h3>$60</h3>
           <h4>per month</h4>
           <button class="btn btn-lg" onclick="checkout(1)">Sign Up</button>
         </div>
@@ -125,15 +127,16 @@
         <div class="panel-heading">
           <h1>Pro</h1>
         </div>
-        <div class="panel-body">
-          <p><strong>50</strong> Lorem</p>
-          <p><strong>25</strong> Ipsum</p>
-          <p><strong>10</strong> Dolor</p>
-          <p><strong>5</strong> Sit</p>
+        <div class="panel-body" style="height: 558px;">
+          <img  src="images/box-2.png" style="width: 400px; padding-bottom: 55px;">
+          <p><strong>20</strong> Lorem</p>
+          <p><strong>15</strong> Ipsum</p>
+          <p><strong>5</strong> Dolor</p>
+          <p><strong>2</strong> Sit</p>
           <p><strong>Endless</strong> Amet</p>
         </div>
         <div class="panel-footer">
-          <h3>$29</h3>
+          <h3>$150</h3>
           <h4>per month</h4>
           <button class="btn btn-lg" onclick="checkout(2)">Sign Up</button>
         </div>
@@ -145,14 +148,15 @@
           <h1>Premium</h1>
         </div>
         <div class="panel-body">
-          <p><strong>100</strong> Lorem</p>
-          <p><strong>50</strong> Ipsum</p>
-          <p><strong>25</strong> Dolor</p>
-          <p><strong>10</strong> Sit</p>
-          <p><strong>Endless</strong> Amet</p>
+          <img  src="images/box-3.png" style="width: 400px; padding-bottom: 10px;">
+          <p><strong>20</strong> Lorem</p>
+          <p><strong>15</strong> Ipsum</p>
+          <p><strong>5</strong> Dolor</p>
+          <p><strong>2</strong> Sit</p>
+          <p><strong>Endless</strong> Ahmed</p>
         </div>
         <div class="panel-footer">
-          <h3>$49</h3>
+          <h3>$300</h3>
           <h4>per month</h4>
           <button class="btn btn-lg" onclick="checkout(3)">Sign Up</button>
         </div>
